effaec7225061f7f4dfbea57043fe0c4c9a2c17f
Tom Wambold <tom5760@gmail.com>
2020-12-01 18:39:36 -0500

    2020: Run code in release mode.

--- 2020/scripts/run.sh
+++ 2020/scripts/run.sh
@@ -4,8 +4,8 @@ INPUT="day$1/input$2"
 
 if test -f $INPUT
 then
-  cargo run --package "day$1" < "day$1/input$2"
+  cargo run --release --package "day$1" < "day$1/input$2"
 else
   echo no input file
-  cargo run --package "day$1"
+  cargo run --release --package "day$1"
 fi
